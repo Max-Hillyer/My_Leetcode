c49c92f1a96d4180fb288232e55d027bc9bcefbd
Max Hillyer <maxhillyer7012@gmail.com>
2025-06-27 18:39:53 -0400

    Update reverse_int.py

--- Medium/reverse_int.py
+++ Medium/reverse_int.py
@@ -1,1 +1,19 @@
+#max hillyer 6/27/25
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        thelist = [str(i) for i in str(x)]
+        thelist.reverse()
+        if '-' in thelist:
+            thelist.remove('-')
+            print(thelist)
+            neg = True
+        else:
+            neg = False
+        final = int("".join(thelist))
+        if final > 2**31 - 1 or final < -2**31:
+            return 0
+        if neg:
+            return final - 2*final
+        return final
 
